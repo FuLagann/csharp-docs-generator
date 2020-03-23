4e746c6302e60fac294590bc980e55288cbbde0c
Paul G.B <fulagann@gmail.com>
2020-03-22 22:34:09 -0700

    Added the sharp checker type json format

---
+++ src/models/SharpChecker.ts
@@ -0,0 +1,130 @@
+
+export interface QuickTypeInfo {
+	unlocalizedName : string;
+	name : string;
+	fullName : string;
+	namespaceName : string;
+	genericParameters : GenericParametersInfo[];
+}
+
+export interface TypeInfo {
+	typeInfo : QuickTypeInfo;
+	assemblyName : string;
+	isDelegate : boolean;
+	isNested : boolean;
+	accessor : string;
+	modifier : string;
+	objectType : string;
+	declaration : string;
+	fullDeclaration : string;
+	baseType : QuickTypeInfo;
+	attributes : AttributeInfo[];
+	interfaces : QuickTypeInfo[];
+	constructors : MethodInfo[];
+	fields : FieldInfo[];
+	staticFields : FieldInfo[];
+	properties : PropertyInfo[];
+	staticProperties : PropertyInfo[];
+	events : EventInfo[];
+	staticEvents : EventInfo[];
+	methods : MethodInfo[];
+	staticMethods : MethodInfo[];
+	operators : MethodInfo[];
+}
+
+export interface GenericParametersInfo {
+	unlocalizedName : string;
+	name : string;
+	constraints : QuickTypeInfo[];
+}
+
+export interface AttributeFieldInfo {
+	name : string;
+	value : string;
+	typeInfo : QuickTypeInfo;
+}
+
+export interface AttributeInfo {
+	typeInfo : QuickTypeInfo;
+	constructorArgs : AttributeFieldInfo[];
+	properties : AttributeFieldInfo[];
+	parameterDeclaration : string;
+	fullDeclaration : string;
+}
+
+export interface FieldInfo {
+	name : string;
+	value : string;
+	isConstant : boolean;
+	isStatic : boolean;
+	isReadonly : boolean;
+	attributes : AttributeInfo[];
+	accessor : string;
+	modifier : string;
+	typeInfo : QuickTypeInfo;
+	implementedType : QuickTypeInfo;
+	declaration : string;
+}
+
+export interface PropertyInfo {
+	name : string;
+	isStatic : boolean;
+	hasGetter : boolean;
+	hasSetter : boolean;
+	attributes : AttributeInfo[];
+	accessor : string;
+	modifier : string;
+	typeInfo : QuickTypeInfo;
+	implementedType : QuickTypeInfo;
+	parameters : ParameterInfo[];
+	getter : MethodInfo;
+	setter : MethodInfo;
+	declaration : string;
+	parameterDeclaration : string;
+	getSetDeclaration : string;
+	fullDeclaration : string;
+}
+
+export interface EventInfo {
+	name : string;
+	isStatic : boolean;
+	accessor : string;
+	modifier : string;
+	typeInfo : QuickTypeInfo;
+	implementedType : QuickTypeInfo;
+	adder : MethodInfo;
+	remover : MethodInfo;
+	fullDeclaration : string;
+}
+
+export interface MethodInfo {
+	name : string;
+	accessor : string;
+	modifier : string;
+	isAbstract : boolean;
+	isConstructor : boolean;
+	isConversionOperator : boolean;
+	isExtension : boolean;
+	isOperator : boolean;
+	isOverriden : boolean;
+	isStatic : boolean;
+	isVirtual : boolean;
+	implementedType : QuickTypeInfo;
+	returnType : QuickTypeInfo;
+	attributes : AttributeInfo[];
+	parameters : ParameterInfo[];
+	declaration : string;
+	parameterDeclaration : string;
+	fullDeclaration : string;
+}
+
+export interface ParameterInfo {
+	name : string;
+	defaultValue : string;
+	attributes : AttributeInfo[];
+	modifier : string;
+	isOptional : boolean;
+	typeInfo : QuickTypeInfo;
+	genericParameterDeclarations : string[];
+	fullDeclaration : string;
+}
