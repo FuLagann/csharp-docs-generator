62d7dfb9f034c8f4d1f4bafdec6601770e6b3698
Paul G.B <fulagann@gmail.com>
2020-04-07 19:52:01 -0700

    Checking outterhtml + children

--- src/read-xml.ts
+++ src/read-xml.ts
@@ -96,10 +96,16 @@ function gatherNameDescriptionList(members : (HTMLCollectionOf<Element> | NodeLi
 	// Variables
 	let results : NameDescription[] = [];
 	
+	io.mkdirP(TEMP_FOLDER + "debugging/");
 	for(let i = 0; i < members.length; i++) {
 		// Variables
 		const name = members[i].getAttribute(attrName);
-		let desc = (members[i].innerHTML || "No description").trim() + ".";
+		let desc = (members[i].textContent || "No description").trim() + ".";
+		
+		fs.appendFileSync(TEMP_FOLDER + "debugging/debug.txt", "\tOutter: " + members[i].outerHTML);
+		for(let a = 0; a < members[i].children.length; a++) {
+			fs.appendFileSync(TEMP_FOLDER + "debugging/debug.txt", "\t\tChildren: " + members[i].children[a].outerHTML);
+		}
 		
 		if(desc.endsWith("..")) { desc = desc.substring(0, desc.length - 1); }
 		if(!name) { continue; }
@@ -122,7 +128,7 @@ function getTextContent(member : Element, id : string, defaultText : string) : s
 	if(elems.length == 0) { return defaultText; }
 	
 	// Variables
-	let desc = (elems[0].innerHTML || defaultText).trim() + ".";
+	let desc = (elems[0].textContent || defaultText).trim() + ".";
 	
 	if(desc.endsWith("..")) { desc = desc.substring(0, desc.length - 1); }
 	
